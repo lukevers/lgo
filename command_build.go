ee99a05fe527e59b7f4407df6d7b70b59bbf696e
Luke Evers <luke@lukevers.com>
2016-07-30 10:59:06 -0400

    Compile all lgo files in the current directory to go

--- command_build.go
+++ command_build.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"github.com/urfave/cli"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func CommandBuild(c *cli.Context) error {
-	file, err := os.Open("examples/basic/main.lgo")
+	files, err := filepath.Glob("*.lgo")
 	if err != nil {
 		return err
 	}
 
-	p, err := NewParser(file).Parse()
-	if err != nil {
-		return err
-	}
+	for _, path := range files {
+		file, err := os.Open(path)
+		defer file.Close()
+		if err != nil {
+			return err
+		}
 
-	fmt.Println(p)
+		parsed, err := NewParser(file).Parse()
+		if err != nil {
+			return err
+		}
+
+		npath := strings.Replace(path, ".lgo", ".go", 1)
+		nfile, err := os.Create(npath)
+		if err != nil {
+			return err
+		}
+
+		nfile.WriteString(parsed)
+		nfile.Close()
+	}
 
 	return nil
 }
